e6e39f7991d31d500187c0a6db2c69cee6784421
forxtoisate <hwoolford@gmail.com>
2024-03-18 18:39:37 -0500

    Initial JWT functionality implementation

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const dotenv = require('dotenv');
+const { authMiddleware } = require('./utils/auth')
 
 dotenv.config();
 
@@ -30,7 +31,9 @@ const startApolloServer = async () => {
     });
   }
   
-  app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
   db.once('open', () => {
     app.listen(PORT, () => {
