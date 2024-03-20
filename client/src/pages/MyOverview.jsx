d251a14fef30168c7bc62d9c17e10b290e7d1667
Sam Brez <sambreslin02@gmail.com>
2024-03-19 20:19:50 -0400

    fix merge conflict

--- client/src/pages/MyOverview.jsx
+++ client/src/pages/MyOverview.jsx
@@ -3,7 +3,7 @@ import ProjectList from "../components/ProjectsList/projectsList";
 export default function MyOverview() {
 
     return(
-        <div>
+        <>
         <h1>Overview</h1>
         <ProjectList />
         </>
