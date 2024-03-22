2be4b4db0b4d3d97ba4480c83084b81639582114
forxtoisate <hwoolford@gmail.com>
2024-03-22 17:55:11 -0500

    Updated profile layout

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,22 @@
-import ProfileForm from "../components/ProfileForm/ProfileForm.jsx"
+import ProfileForm from "../components/ProfileForm/ProfileForm.jsx";
+import { Grid, Typography } from "@mui/material";
 
 export default function Profile() {
-
-    return(
-        <>
-        <h1>Profile Settings</h1>
-        <ProfileForm />
-        </>
-    )
+  return (
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        alignItems="center"
+        justifyContent="space-around">
+        <Grid item sx={{ py: 2 }}>
+          <Typography variant="h2" align="center" gutterBottom>
+            Profile Settings
+          </Typography>
+        </Grid>
+        <Grid item sx={{ py: 2 }}>
+          <ProfileForm />
+        </Grid>
+      </Grid>
+  );
 }
