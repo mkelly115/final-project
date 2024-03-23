60fa32e1d9630b7c58437d92b54277b67747bfa7
forxtoisate <hwoolford@gmail.com>
2024-03-23 12:34:32 -0500

    Moved addproject button and projectlist to company overview

--- client/src/pages/MyProject.jsx
+++ client/src/pages/MyProject.jsx
@@ -1,25 +1,11 @@
 // import ProjectList from "../components/ProjectsList/projectsList.jsx"
-import ProjectForm from "../components/ProjectForm/ProjectForm";
-import { useState } from "react";
-import { Button } from "@mui/material";
 import MyProjectList from "../components/MyProject/MyProjectList"
 
 export default function MyProject() {
-  const [open, setOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setOpen(false);
-  };
   return (
     <>
     <h1>My Projects</h1>
     <MyProjectList />
-      <Button onClick={handleOpenModal}>Add Project</Button>
-      <ProjectForm open={open} handleClose={handleCloseModal} />
     </>
   );
 }
