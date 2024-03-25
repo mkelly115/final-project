e8d7a27b3489b31fda297f205ee172d2bb86a4bc
michael <mikek222@gmail.com>
2024-03-26 02:23:09 +0300

    teamprojects page responsiveness

--- client/src/components/TeamTaskList/TeamTaskList.jsx
+++ client/src/components/TeamTaskList/TeamTaskList.jsx
@@ -1,34 +1,30 @@
-import { Table, TableContainer, TableHead, TableBody, TableCell, TableRow, Paper } from '@mui/material';
+import { Table, TableContainer, TableHead, TableBody, TableCell, TableRow, Paper, Typography } from '@mui/material';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 
-
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#a6d8aa', 
-      },
+  palette: {
+    primary: {
+      main: '#c55a48 ',
     },
-  });
-  
-  const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.common.white,
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  }));
+  },
+});
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.common.white,
+}));
 
-
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}));
 
 const TeamTaskList = ({ tasks }) => {
   return (
     <ThemeProvider theme={theme}>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="task table">
+      <TableContainer component={Paper} sx={{ overflowX: 'auto' }}>
+        <Table aria-label="task table">
           <TableHead>
             <StyledTableRow>
               <StyledTableCell>Description</StyledTableCell>
@@ -37,15 +33,23 @@ const TeamTaskList = ({ tasks }) => {
             </StyledTableRow>
           </TableHead>
           <TableBody>
-            {tasks.map((task) => (
-              <StyledTableRow key={task._id}>
-                <TableCell component="th" scope="row">
-                  {task.description}
+            {tasks.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={3} align="center">
+                  <Typography variant="body1">No tasks available.</Typography>
                 </TableCell>
-                <TableCell>{task.taskStatus}</TableCell>
-                <TableCell>{task.dateDue}</TableCell>
-              </StyledTableRow>
-            ))}
+              </TableRow>
+            ) : (
+              tasks.map((task) => (
+                <StyledTableRow key={task._id}>
+                  <TableCell component="th" scope="row">
+                    {task.description}
+                  </TableCell>
+                  <TableCell>{task.taskStatus}</TableCell>
+                  <TableCell>{task.dateDue}</TableCell>
+                </StyledTableRow>
+              ))
+            )}
           </TableBody>
         </Table>
       </TableContainer>
