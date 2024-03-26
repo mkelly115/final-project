02c30b38f0dcce24a6e6f2a9d8a92f797a7fd552
michael <mikek222@gmail.com>
2024-03-26 20:34:02 +0300

    responsive and style changes made to MyProjects, MyTasks, and TeamTasks

--- client/src/components/MyProject/MyProjectList.jsx
+++ client/src/components/MyProject/MyProjectList.jsx
@@ -1,4 +1,3 @@
-// Import the necessary dependencies
 import { useQuery } from "@apollo/client";
 import { Link as RouterLink } from "react-router-dom";
 import { QUERY_ME, QUERY_SINGLE_USER } from "../../utils/queries";
@@ -8,9 +7,40 @@ import {
   CardContent,
   CardActionArea,
   Typography,
+  Avatar,
 } from "@mui/material";
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import webpulselogo from "/webpulselogo.png";
 
-// Inside your component where you want to fetch and display the projects
+const theme = createTheme({
+  typography: {
+    h5: {
+      fontSize: "1.5rem", // Default font size
+      "@media (max-width:1200px)": {
+        fontSize: "1.2rem", // Decrease font size on smaller screens
+      },
+    },
+    subtitle1: {
+      fontSize: "1rem", // Default font size
+      "@media (max-width:1200px)": {
+        fontSize: "0.8rem", // Decrease font size on smaller screens
+      },
+    },
+  },
+});
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  width: "100%",
+  borderRadius: 8,
+  border: `3px solid #e4442b`, 
+  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)", 
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+  height: "100%",
+}));
+
+
 const MyProjectList = () => {
   const {
     loading: meLoading,
@@ -18,48 +48,59 @@ const MyProjectList = () => {
     data: meData,
   } = useQuery(QUERY_ME);
 
-  // Query to fetch projects
   const { loading, error, data: projectsData } = useQuery(QUERY_SINGLE_USER, {
-    variables: { userId: meData?.me?._id }, // Pass the logged-in user's ID as a variable
+    variables: { userId: meData?.me?._id },
   });
 
-  // Handle loading and error states
   if (meLoading || loading) return <p>Loading...</p>;
   if (meError || error) return <p>Error fetching data...</p>;
-
-  // Verify that meData and projectsData are populated
   if (!meData || !projectsData || !projectsData.user || !projectsData.user.projects) return <p>No user data or projects found...</p>;
 
-console.log(projectsData.user.projects);
-const projects = projectsData.user.projects;
+  const projects = projectsData.user.projects;
 
   return (
-    <Grid container spacing={2} sx={{ justifyContent: "center" }}>
-      {projects.map((project) => (
-        <Grid item key={project._id}>
-          <Card variant="outlined" sx={{ width: 400 }}>
-            <CardActionArea
-              component={RouterLink}
-              to={`/dashboard/projects/${project._id}`}
-              underline="none"
-              sx={{ display: "block" }}
-            >
-              <CardContent>
-                <Typography variant="h5" component="div" gutterBottom>
-                  {project.name}
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                  Status: {project.projectStatus}
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                  Due Date: {project.dateDue}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+        {projects.map((project) => (
+          <Grid item key={project._id}>
+            <StyledCard variant="outlined">
+              <Avatar
+                alt="WebPulse Logo"
+                src={webpulselogo}
+                sx={{ width: "5rem", height: "5rem", alignSelf: "center", marginTop: 1 }}
+              />
+              <CardActionArea
+                component={RouterLink}
+                to={`/dashboard/projects/${project._id}`}
+                underline="none"
+                sx={{ display: "block" }}
+              >
+                <CardContent>
+                  <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: "bold" }}>
+                    {project.name}
+                  </Typography>
+                  <Typography variant="subtitle1" gutterBottom sx={{ fontStyle: "italic", fontWeight: "bold", color: "#e4442b" }}>
+                    Status: {project.projectStatus}
+                  </Typography>
+                  <Typography variant="subtitle2" gutterBottom sx={{ fontWeight: "bold" }}>
+                    <ul style={{ listStyleType: "none", padding: 0, display: "flex", flexDirection: "row", justifyContent: "center" }}>
+                      {project.team && Array.isArray(project.team) && project.team[0].members.map((member, index) => (
+                        <React.Fragment key={member._id}>
+                          {index !== 0 && (
+                            <li style={{ display: "inline", margin: "0 5px" }}>&nbsp;|&nbsp;</li>
+                          )}
+                          <li>{member.firstName} {member.lastName}</li>
+                        </React.Fragment>
+                      ))}
+                    </ul>
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </StyledCard>
+          </Grid>
+        ))}
+      </Grid>
+    </ThemeProvider>
   );
 };
 
--- client/src/components/MyTasks/MyTasks.jsx
+++ client/src/components/MyTasks/MyTasks.jsx
@@ -1,83 +1,89 @@
 import { useQuery } from "@apollo/client";
 import { QUERY_ME, QUERY_TASKS } from "../../utils/queries";
 import {
-    Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
 } from "@mui/material";
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect } from "react";
 
 // Create a custom theme
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#a6d8aa', // Set primary color to #a6d8aa
-      },
+  palette: {
+    primary: {
+      main: '#E4442B',
     },
-  });
-  
-  // StyledTableCell and StyledTableRow components for custom styling
-  const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.common.white,
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  }));
+  },
+});
+
+// StyledTableCell and StyledTableRow components for custom styling
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.common.white,
+  [theme.breakpoints.down('sm')]: {
+    padding: "8px", // Reduce padding for smaller screens
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}));
 
 const MyTaskList = () => {
-  // const { loading: meLoading, error: meError, data: meData } = useQuery(QUERY_ME);
-
   const { loading: meLoading, error: meError, data: meData, refetch: refetchMe } = useQuery(QUERY_ME);
-
-  // const { loading: tasksLoading, error: tasksError, data: tasksData } =
-  //   useQuery(QUERY_TASKS);
-
   const { loading: tasksLoading, error: tasksError, data: tasksData, refetch: refetchTask } =
     useQuery(QUERY_TASKS);
 
-    useEffect(() => {
-      refetchMe();
-      refetchTask();
-    }, [refetchMe, refetchTask]);
-
-
+  useEffect(() => {
+    refetchMe();
+    refetchTask();
+  }, [refetchMe, refetchTask]);
 
   if (meLoading || tasksLoading) return <p>Loading...</p>;
   if (meError || tasksError) return <p>Error fetching data...</p>;
   if (!meData || !tasksData || !tasksData.tasks)
     return <p>No user data or tasks found...</p>;
 
-const userTasks = tasksData.tasks;
+  const userTasks = tasksData.tasks;
 
   return (
     <ThemeProvider theme={theme}>
-    <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} aria-label="task table">
-      <TableHead>
-        <StyledTableRow>
-          <StyledTableCell>Description</StyledTableCell>
-          <StyledTableCell>Status</StyledTableCell>
-          <StyledTableCell>Due Date</StyledTableCell>
-        </StyledTableRow>
-      </TableHead>
-      <TableBody>
-        {userTasks.map((task) => (
-          <StyledTableRow key={task._id}>
-            <TableCell component="th" scope="row">
-              {task.description}
-            </TableCell>
-            <TableCell>{task.taskStatus}</TableCell>
-            <TableCell>{task.dateDue}</TableCell>
-          </StyledTableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
-  </ThemeProvider>
-      );
-    };
+      <TableContainer component={Paper}>
+        <Table aria-label="task table">
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell>Description</StyledTableCell>
+              <StyledTableCell>Status</StyledTableCell>
+              <StyledTableCell>Due Date</StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {userTasks.map((task) => (
+              <StyledTableRow key={task._id}>
+                <TableCell>
+                  <Typography variant="body1">{task.description}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">{task.taskStatus}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">{task.dateDue}</Typography>
+                </TableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </ThemeProvider>
+  );
+};
 
 export default MyTaskList;
